7a8b6f70af2dfa46d3baf39290a90768f21b9201
Terry Smith <Terry.smith.s1kr@gmail.com>
2021-10-21 13:45:05 +1030

    Added route to the collection view

---
+++ Kraken.AccountInfo/Kraken.AccountInfo/Views/Converters/StringCollectionConverter.cs
@@ -0,0 +1,30 @@
+﻿using QuikGraph;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Kraken.AccountInfo
+{
+    public class StringCollectionConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var collection = value as IEnumerable<Edge<string>>;
+            var sb = new StringBuilder();
+            foreach (var item in collection)
+            {
+                if (sb.Length == 0)
+                    sb.Append(item.Source);
+                sb.Append($" -> {item.Target}");
+            }
+            return sb.ToString();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
